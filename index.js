a90e2080b10f3284f07c2a0dcea71d61adf14561
Kamau <kamauwamuigaijr@gmail.com>
2024-03-19 15:32:00 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function saturdayFun(activity="roller-skate") { //create actual function
+    return `This Saturday, I want to ${activity}!` //display after no arguments are passed?? i think
+  } 
+  let mondayWork = function(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+  }
+
+  let wrapAdjective = function(style="*") {
+    return function(adjective="special") {
+      return `You are ${style}${adjective}${style}!`
+    }
+  }
